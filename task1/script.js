ffb6cb5c80e089e8c0c75b23c105b2337e8da89e
naijatechgirle <ruthiekelly38@gmail.com>
2022-06-24 00:46:13 +0100

    javascript code to print my details

---
+++ task1/script.js
@@ -0,0 +1,7 @@
+function prints() {
+    let feet = 5;
+    let nameVal = 'Eluagu Kelechi Ruth';
+    let height = `${feet} feet`;
+    let country = 'Nigeria';
+    alert(`My name is ${nameVal}, I am ${height} in height, I live in ${country} with my family.`)
+}
