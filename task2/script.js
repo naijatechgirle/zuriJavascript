a8a92f4499a639e630742dd4f87c21b488a525a3
naijatechgirle <ruthiekelly38@gmail.com>
2022-06-24 20:10:50 +0100

    added a function

--- task2/script.js
+++ task2/script.js
@@ -1,13 +1,9 @@
-function arithmetic() {
-    let num1 = Number(prompt('Enter first number'));
-    let operator = prompt('Enter operator (+, -, *, /)');
-    let num2 = Number(prompt('Enter second number'));
-    let result;
+let num1 = Number(prompt('Enter first number'));
+let operator = prompt('Enter operator (+, -, *, /)');
+let num2 = Number(prompt('Enter second number'));
+let result; 
 
-    if(operator != '+' || operator != '-' || operator != '*' || operator != '/') {
-        alert('comarade, use only any of the 4 operators na 😐');
-    };
-
+function arithmetic() { 
     if (operator == '+') {
         result = num1 + num2;
     } else if (operator == '-'){
@@ -17,6 +13,6 @@ function arithmetic() {
     } else if (operator == '/') {
         result = num1 / num2;
     }
-    alert(`'The result of your simple arithmetic calculation is ' ${result}`)
+    alert(`The result of your simple arithmetic calculation is ${result}`)
 }
 arithmetic();
