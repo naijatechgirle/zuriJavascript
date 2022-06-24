cf7d9cd030a1af3f38e84744e5e500fcf96f353e
naijatechgirle <ruthiekelly38@gmail.com>
2022-06-24 01:15:47 +0100

    added a javascript built in function

--- task1/script.js
+++ task1/script.js
@@ -3,5 +3,6 @@ function prints() {
     let nameVal = 'Eluagu Kelechi Ruth';
     let height = `${feet} feet`;
     let country = 'Nigeria';
-    alert(`My name is ${nameVal}, I am ${height} in height, I live in ${country} with my family.`)
+    alert(`My name is ${nameVal}, I am ${height} in height, I live in ${country} with my family.`);
+    console.log(`My name is ${nameVal}, I am ${height} in height, I live in ${country} with my family.`);
 }
